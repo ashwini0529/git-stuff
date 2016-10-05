98481563e30da75efc44fd6e0f89c8973beb75a3
hkmangla <hemantmangla78@gmail.com>
2016-10-06 02:29:38 +0530

    Fix errors and watching function

--- gitfiles.py
+++ gitfiles.py
@@ -12,6 +12,19 @@ def infoAboutRepo():
 	Given username and repository, Returns information about the repository.
 	"""
 	user = raw_input('Enter the user name ')
+	url = 'https://github.com/'+user
+	# Check If username is invalid
+	try:
+		soup = BeautifulSoup(urllib2.urlopen(url).read())
+	except Exception:
+		print 'User "%s" does not exist! Please try again.' %(user)
+		exit()
+	
+	popularRepo = soup.find_all('span' , {'class': 'repo js-repo'})
+	print "These are the some popular repo of user",user
+	for repo in popularRepo:
+		print repo.string
+
 	repo = raw_input('Enter the repository name : ')
 	url = "https://github.com/"+user+'/'+repo
 	try:
@@ -24,16 +37,16 @@ def infoAboutRepo():
 	def pulse(url):
 		"""
 		"""
-		url+= '/pulse/monthly'
+		url += '/pulse/monthly'
 		page = urllib2.urlopen(url)
 		soup = BeautifulSoup(page.read())
 		div_all = soup.findAll('div',{'class':'section diffstat-summary'})
 		if not div_all:
 			print 'No Recent activities in the repository.'
 			return
-		print 'The whole information about the repository is as follows :\n'
+		print '\nThe whole information about the repository is as follows :\n'
 		for each_div in div_all:
-		    print each_div.get_text()
+		    print ' '.join(each_div.get_text().split())
 
 	def readme(url):
 		"""
@@ -47,7 +60,7 @@ def infoAboutRepo():
 			print 'ReadMe file for the repository doesn\'t exist'
 			return
 
-		print 'README\n'
+		print '\nREADME\n'
 		print paragraphs
 
 
@@ -56,8 +69,8 @@ def infoAboutRepo():
 		"""
 		# TODO: watching not working as of now. Only giving 0 as Watcher...
 		soup = BeautifulSoup(urllib2.urlopen(url).read())
-		watch = soup.find('a' , {"class" : "social-count js-social-count"}).get_text()
-		print 'Watchers: %s' %(watch)
+		watch = soup.find('a' , {"class" : "social-count js-social-count"}).text
+		print 'Watchers: %s' %(watch.split()[0])
 
 
 	def statistics(url):
@@ -68,15 +81,16 @@ def infoAboutRepo():
 		if not ultags_all:
 			print 'No activities in the repository.'
 			return
-
+		print "\nUsers Activities in Repo"
 		for ultag in ultags_all :
 			for litag in ultag.find_all('li'):
-				print litag.text
+				if ' '.join(litag.text.split()) != "Fetching contributors":
+					print ' '.join(litag.text.split())
 
 	statistics(url)
 	pulse(url)
 	readme(url)
-	#watching(url) ---> Now not showing correct watch number.. always 0
+	watching(url)
 	#more features to be added...
 
 
@@ -102,74 +116,83 @@ def infoAboutUser():
 
 		#Give users full name
 		fullName = soup.find('span', attrs = {'class': "vcard-fullname"}).text
-		print "\nFull name: ",fullName
+		print "Full name: ",fullName
 		
 		#Give users username
 		userName = soup.find('span', attrs = {'class': "vcard-username"}).text
-		print "\nusername: ",userName
+		print "username: ",userName
 		
 		#Give users home town
 		try:
 			homeTown = soup.find('li',{'aria-label':"Home location"}).text
-			print "\nHome Town: ",homeTown
+			print "Home Town: ",homeTown
 		except:
-			print "\nUser does not add his/her hometown on github!"
+			print "User does not add his/her hometown on github!"
 		#Give user Email-Id
 		try:
 			email_id = soup.find('li',{'aria-label' : "Email"}).text
-			print "\nemail-id: ",email_id
+			print "email-id: ",email_id
 		except:
-			print "\nUser does not add his/her email-id on github!"
+			print "User does not add his/her email-id on github!"
 			
 		#Give Joining date
 		join = soup.find('li',{'aria-label':"Member since" }).text
-		print "\nJoining date of github: ",join[10:]
+		print "Joining date of github: ",join[10:]
 		
 		#Give users oraginsation 
 		try:
 			organization = soup.find('li',{'aria-label' : "Organization"}).text
-			print "\nOrganization: ",organization
+			print "Organization: ",organization
 		except:
-			print "\nUser does not add his/her working Organization on github!"
+			print "User does not add his/her working Organization on github!"
 
 		#Give users Blog or Website 
 		try:
 			website = soup.find('li',{'aria-label' : "Blog or website"}).text
-			print "\nPersonal website: ",website
+			print "Personal website: ",website
 		except:
-			print "\nUser does not add his/her personal website on github!\n"
+			print "User does not add his/her personal website on github!"
 		
 	def contributions(soup):
 		"""
 		Returns the contributions done by user in given Period.
 		"""
 		# TODO: Generates error. Needs modification
-		totalContributions = soup.find('div' , {'class' : 'contrib-column contrib-column-first table-column'}).get_text()
-		print totalContributions
-		longestStreak = soup.find('div' , {'class' : 'contrib-column table-column'}).get_text()
-		print longestStreak	
-		h3 = soup.find('h3', 'conversation-list-heading')
-		spans = h3.find_all('span', attrs = {'class': "text-emphasized"})
-		for span in spans:
-			print 'Total commits this week : '+ span.string
+		print "\nContributions of User\n"
+
+		totalContributions = soup.find('div' , {'class' : 'js-contribution-graph'}).find('h2',{'class' : 'f4 text-normal mb-3'}).text
+		print "Total contributions last year",totalContributions.split()[0]
+
+
+		Streaks = soup.find('svg' , {'class' : 'js-calendar-graph-svg'}).find_all('rect')
+		longestStreak = 0
+		streakList = []
+		for streak in Streaks:
+			streakList.append(int(streak['data-count']))
+			longestStreak = max(int(streak['data-count']),longestStreak)	
+		print "Longest Streak: ",longestStreak
+
+		print "Total contributions last weeks: ",sum(streakList[-7:])
 
 
 	def popularRepos(soup):
 		"""
 		Returns Public repositories of the user.
 		"""
-		popularRepo = soup.find('div' , {'class': 'boxed-group flush'})
-		spans = popularRepo.find_all('span', attrs = {'class' : 'repo'})
-		if not spans:
+		popularRepo = soup.find_all('span' , {'class': 'repo js-repo'})
+
+		if not popularRepo:
 			print 'No public repositories for the given user.'
 			return
 		countPopularRepo =0
-		for span in spans:
+		for repo in popularRepo:
 			countPopularRepo = countPopularRepo+1
-			print str(countPopularRepo)+' : '+span.string
+			print str(countPopularRepo)+' : '+repo.string
 
+	print "\nUsers Info\n"
 	profileInfo(soup)
 	contributions(soup)
+	print "\nUsers Popular Repositories\n"
 	popularRepos(soup)
 
 if __name__ == "__main__":
